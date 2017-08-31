699cdba8c84a41df54e709d375fcbd58e3b86f6e
ChefDK User <spandox@spandox.com>
2017-08-31 18:38:34 +0000

    more tests

--- cookbooks/apache/test/smoke/default/default_test.rb
+++ cookbooks/apache/test/smoke/default/default_test.rb
@@ -6,13 +6,15 @@
 # found at http://inspec.io/docs/reference/resources/
 
 unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
+  describe user('root') do
     it { should exist }
   end
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(80) do
+  it { should be_listening }
+end
+
+describe command('curl localhost') do
+  its('stdout') { should match('Hello, world') }
 end
